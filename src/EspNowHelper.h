8119df8ba90505c8cc93b6d2ed3008be18e82b07
jreue <jreue@users.noreply.github.com>
2026-01-14 16:54:08 -0600

    Add tm-shared library and refactor message structs
    Added tm-shared as a dependency in platformio.ini. Refactored EspNowHelper.h to use MessageStructs from tm-shared, removing local message type definitions and DateMessage struct.

--- src/EspNowHelper.h
+++ src/EspNowHelper.h
@@ -1,23 +1,9 @@
 #pragma once
 
 #include <Arduino.h>
+#include <MessageStructs.h>
 #include <WiFi.h>
 #include <esp_now.h>
-
-// Message types
-#define MSG_TYPE_CONNECT 0
-#define MSG_TYPE_STATUS 1
-#define MSG_TYPE_DISCONNECT 2
-#define MSG_TYPE_DATE_UPDATE 3
-
-typedef struct DateMessage {
-    uint8_t id;
-    uint8_t messageType;  // MSG_TYPE_CONNECT, MSG_TYPE_STATUS, MSG_TYPE_DISCONNECT,
-                          // MSG_TYPE_DATE_UPDATE
-    uint8_t month;
-    uint8_t day;
-    uint16_t year;
-} DateMessage;
 
 class EspNowHelper {
   public:
